cb7c6ffdd880acc1c8cd02984deb07d7b046b800
blad <64702883+blad-dev@users.noreply.github.com>
2026-01-28 11:03:12 +0100

    Add a function for calculating single operations

--- logic.js
+++ logic.js
@@ -1,1 +1,26 @@
 console.log('Hello, world!');
+
+function toNumber(value) {
+  let number = Number(value);
+  if (isNaN(number)) {
+    throw TypeError(`operand is not a number: ${value}`);
+  }
+  return number;
+}
+function calcSingleOperation(leftOperand, operation, rightOperand) {
+  leftOperand = toNumber(leftOperand);
+  rightOperand = toNumber(rightOperand);
+
+  switch (operation) {
+    case '+':
+      return leftOperand + rightOperand;
+    case '-':
+      return leftOperand - rightOperand;
+    case '*':
+      return leftOperand * rightOperand;
+    case '/':
+      return leftOperand / rightOperand;
+    default:
+      throw TypeError(`invalid operation: ${operation}`);
+  }
+}
