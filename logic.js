3f083dfcbc9aeccc1e2ef35643bb106a489ee1db
blad <64702883+blad-dev@users.noreply.github.com>
2026-01-29 15:01:40 +0100

    Add routing for pressing a button

--- logic.js
+++ logic.js
@@ -24,3 +24,32 @@ function calcSingleOperation(leftOperand, operation, rightOperand) {
       throw TypeError(`invalid operation: ${operation}`);
   }
 }
+
+function handleClick(event) {
+  const textContent = event.target.textContent;
+  // prettier-ignore
+  switch(textContent){
+    case '0': case '1': case '2': case '3': case '4': 
+    case '5': case '6': case '7': case '8': case '9':
+      handleNumberPressed(textContent);
+      break;
+    case '+': case '-': case '*': case '/':
+      handleOperatorPressed(textContent);
+      break;
+    case '.':
+      handleDotPressed(textContent);
+      break;
+    case 'e':
+      handleConstantPressed(textContent);
+      break;
+    case '=':
+      handleEqualsPressed();
+      break;
+    case 'CLEAR':
+      handleCLEARPressed();
+      break;
+  }
+}
+const calculator = document.querySelector('div.calculator');
+const numberDisplay = calculator.querySelector('input.number-display');
+calculator.addEventListener('click', handleClick);
